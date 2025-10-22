53070d87e18bf6e3f567965b524c9c9bc2bb26e9
GuilhermeCF <guicostaferreira.2003@gmail.com>
2025-10-22 22:33:57 +0100

    Add intrinsic camera data extraction functionality and remove old add-on

---
+++ blender_simulator/add-ons/instrinic.py
@@ -0,0 +1,64 @@
+import bpy
+import json
+import os
+
+import numpy as np
+
+
+
+scene = bpy.context.scene
+cam = scene.camera.data
+
+# Get render resolution and scale
+res_x = scene.render.resolution_x
+res_y = scene.render.resolution_y
+scale = scene.render.resolution_percentage / 100
+
+# Effective resolution in pixels
+res_x_px = res_x * scale
+res_y_px = res_y * scale
+
+# Camera parameters
+f_in_mm = cam.lens
+sensor_width_in_mm = cam.sensor_width
+sensor_height_in_mm = cam.sensor_height
+
+# Pixel aspect ratio (can be ≠ 1 for anamorphic)
+pixel_aspect = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
+
+# Adjust sensor size for sensor fit
+if cam.sensor_fit == 'VERTICAL':
+    s_u = res_x_px / (sensor_width_in_mm * pixel_aspect)
+    s_v = res_y_px / sensor_height_in_mm
+else:  # 'HORIZONTAL' or 'AUTO'
+    s_u = res_x_px / sensor_width_in_mm
+    s_v = res_y_px * pixel_aspect / sensor_height_in_mm
+
+# Focal length in pixels
+f_x = f_in_mm * s_u
+f_y = f_in_mm * s_v
+
+# Principal point (usually center)
+c_x = res_x_px / 2.0
+c_y = res_y_px / 2.0
+
+# Intrinsic matrix
+K = np.array([[f_x, 0, c_x],
+              [0, f_y, c_y],
+              [0,   0,   1]])
+
+print("Intrinsic matrix K:\n", K)
+
+output_path = os.path.join(bpy.path.abspath("//"), "camera_intrinsics.json")
+
+# Convert numpy array to plain Python list
+data = {
+    "K": K.tolist(),
+    "resolution": [res_x_px, res_y_px],
+    "focal_mm": f_in_mm
+}
+
+with open(output_path, "w") as f:
+    json.dump(data, f, indent=4)
+
+print(f"Saved intrinsics to {output_path}")
